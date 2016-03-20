d201940e06b448d2bcfdb25cdcfa8573d1ae3ddc
9dev <9devmail@gmail.com>
2016-03-20 12:25:34 +0100

    Created test suite sketch.

---
+++ fts/Ban.py
@@ -0,0 +1,28 @@
+from ._base import BaseTestCase
+
+
+class TestBan(BaseTestCase):
+
+    def test_can_ban_user_permanently(self):
+        self.fail()
+
+    def test_can_ban_user_for_month(self):
+        self.fail()
+
+    def test_can_ban_user_for_week(self):
+        self.fail()
+
+    def test_can_ban_user_for_day(self):
+        self.fail()
+
+    def test_can_warn_user(self):
+        self.fail()
+
+    def test_banned_user_cannot_log_in(self):
+        self.fail()
+
+    def test_banned_user_can_log_in_after_ban_period(self):
+        self.fail()
+
+    def test_user_gets_banned_after_too_many_warnings(self):
+        self.fail()
--- fts/Dummy.py
+++
@@ -1,8 +0,0 @@
-from ._base import BaseTestCase
-
-
-class TestDummy(BaseTestCase):
-
-    def test_dummy(self):
-        self.assertTrue(True)
-
