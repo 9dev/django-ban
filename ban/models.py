75ed37c6cbd77a012aa44e51ad7076e9d84fc5fd
9dev <9devmail@gmail.com>
2016-03-22 09:16:13 +0100

    Allowed ban end date to be blank.

--- ban/models.py
+++ ban/models.py
@@ -8,4 +8,4 @@ USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 class Ban(models.Model):
     receiver = models.ForeignKey(USER_MODEL, unique=True)
     creator = models.ForeignKey(USER_MODEL, related_name='creator')
-    end_date = models.DateTimeField(null=True, default=None)
+    end_date = models.DateTimeField(null=True, blank=True, default=None)
